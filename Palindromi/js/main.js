61dc7adfcac1db4c66cd2b4ec619173ca3556939
Daviduo <davide0400@gmail.com>
2022-11-15 17:06:05 +0100

    Es. parola palindroma fatto

---
+++ Palindromi/js/main.js
@@ -0,0 +1,34 @@
+// Richiesta parola
+let parolaUser = prompt("scrivi una parola per verificare se è palindroma");
+console.log(parolaUser);
+// imposto la costante del risultato e la collego alla funzione
+const risultato = palindroma(parolaUser);
+
+// Creo la funzione
+// Divido la parola nelle sue lettere per poi ricomporla invertita e verifica se coincide con quella iniziale
+    
+    function palindroma(userInput){
+        // Divido la parola in lettere creando un array
+        let worldSplit = userInput.split('');
+        console.log(worldSplit);
+        // Inverto l'array
+        let worldSplitInv = worldSplit.reverse('');
+        console.log(worldSplitInv);
+        // Rimetto insieme la parola invertita
+        var worldInv = worldSplitInv.join('');
+        console.log(worldInv);
+        // Confronto le due parole e riporto il risultato alla costante "risultato" con return
+        if(parolaUser == worldInv){
+            return "la parola digitata è palindroma";
+          } 
+          else {
+            return "la parola digitata non è palindroma";
+          }
+    
+    }
+// Stampo il risultato su terminale
+console.log(risultato);
+    
+
+
+
